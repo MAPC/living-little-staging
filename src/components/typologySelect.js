f2d69a551330dcaf89d7608ebd27e03fa2d48aae
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-04-01 13:23:00 -0400

    Style select dropdown by typology

--- src/components/typologySelect.js
+++ src/components/typologySelect.js
@@ -1,20 +1,47 @@
-import React from "react"
+import React, { useState } from "react"
+import definitions from "../content/definitions.json"
 
-const TypologySelect = () => (
-  <section>
-    <h2 className="typology-select__header">Explore a {' '}</h2>
-    <div className="typology-select__wrapper">
-      <select className="typology-select__selector">
-        <option value="0" className="typology-select__option">accessory dwelling unit</option>
-        <option value="1">tiny house</option>
-        <option value="2">row house</option>
-        <option value="3">small scale mixed-use development</option>
-        <option value="4">cottage development</option>
-        <option value="5">cohousing building</option>
-      </select>
-    </div>
-    <div className="typology-select__shadow"/>
-  </section>
-)
+const TypologySelect = () => {
+  const [typology, setTypology] = useState('adu');
+  const selectWidth = {
+   adu: 225,
+   'tiny house': 100,
+   'row house': 100,
+   'mixed-use': 340,
+   cottage: 210,
+   cohousing: 180,
+  };
+
+  return (
+    <section>
+      <h2 className="typology-select__header">
+        Explore {typology === "adu" ? "an " : "a "}
+      </h2>
+      <div className="typology-select__wrapper">
+        <select className="typology-select__selector"
+          onChange={(event) => setTypology(event.target.value)}
+          style={{
+            width: `${selectWidth[typology] + 30}px`
+          }}
+        >
+          <option value="adu" className="typology-select__option" defaultValue>accessory dwelling unit</option>
+          <option value="tiny house">tiny house</option>
+          <option value="row house">row house</option>
+          <option value="mixed-use">small scale mixed-use development</option>
+          <option value="cottage">cottage development</option>
+          <option value="cohousing">cohousing building</option>
+        </select>
+      </div>
+      <div
+        className="typology-select__shadow"
+        style={{
+          left: typology === "adu" ? "108px" : "98px",
+          width: `${selectWidth[typology]}px`
+        }}
+      />
+      <p>{definitions[typology]}</p>
+    </section>
+  )
+}
 
 export default TypologySelect
