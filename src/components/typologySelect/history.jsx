f81aa0f83e77aa4278f14f15782ba411d6a0c288
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-04-02 12:26:50 -0400

    Render different titles for each opportunity card

--- src/components/typologySelect/history.jsx
+++ src/components/typologySelect/history.jsx
@@ -9,8 +9,14 @@ const History = ({ typology }) => (
     <SubtopicHeader title="History" highlightWidth={85} />
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     <div className="opportunity__wrapper">
-      <OpportunityButton description={opportunties[typology].history.one} />
-      <OpportunityButton description={opportunties[typology].history.two} />
+      <OpportunityButton
+        title={opportunties[typology].history.titleOne}
+        description={opportunties[typology].history.textOne}
+      />
+      <OpportunityButton
+        title={opportunties[typology].history.titleTwo}
+        description={opportunties[typology].history.textTwo}
+      />
     </div>
   </article>
 );
--- src/components/typologySelect/opportunityButton.jsx
+++ src/components/typologySelect/opportunityButton.jsx
@@ -3,17 +3,18 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLightbulb } from '@fortawesome/free-regular-svg-icons';
 
-const OpportunityButton = ({ description }) => (
+const OpportunityButton = ({ title, description }) => (
   <aside className="opportunity">
     <FontAwesomeIcon icon={faLightbulb} className="opportunity__icon" />
     <div>
-      <h4 className="h4 opportunity__title">Opportunity</h4>
+      <h4 className="h4 opportunity__title">{title}</h4>
       {description}
     </div>
   </aside>
 );
 
 OpportunityButton.propTypes = {
+  title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
 };
 
