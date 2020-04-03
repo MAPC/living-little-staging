945c29a0a0da2517aac7743dd6b96403befc10e4
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-04-03 10:50:28 -0400

    Style case studies section

--- src/components/caseStudies.jsx
+++ src/components/caseStudies.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const CaseStudies = () => (
   <section className="section case-studies">
-    <h2 className="h2 main-column">Case studies</h2>
-    <div className="slides__wrapper">
-      <iframe className="slides__embed" src="//slides.com/mapc/deck/embed?style=light" scrolling="no" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+    <div className="main-column case-studies__wrapper">
+      <h2 className="h2 case-studies__title">Case studies</h2>
+      <div className="slides__wrapper">
+        <iframe className="slides__embed" src="//slides.com/mapc/deck/embed?style=light" scrolling="no" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+      </div>
     </div>
   </section>
 );
