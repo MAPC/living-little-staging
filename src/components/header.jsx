27f74923bde1b4ce1a6c4e279753ee2739e31419
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-04-03 15:55:10 -0400

    Add temporary hero image at top

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,28 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import Img from 'gatsby-image';
+import { useStaticQuery, graphql } from 'gatsby';
 
-const Header = ({ siteTitle }) => (
-  <header className="header">
-    {siteTitle}
-  </header>
-);
+const Header = ({ siteTitle }) => {
+  const data = useStaticQuery(graphql`
+  {
+    fileName: file(relativePath: {eq: "temp-hero.png"}) {
+      childImageSharp {
+        fluid(maxWidth: 1920) {
+          src
+        }
+      }
+    }
+  }
+  
+`);
+console.log(data)
+  return (
+    <header className="header" style={{width: "100%"}}>
+      <img src={data.fileName.childImageSharp.fluid.src} style={{width: "100%"}}/>
+    </header>
+  );
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
