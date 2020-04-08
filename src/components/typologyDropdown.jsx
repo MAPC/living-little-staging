c7b98ec53acc25a7e193624b4193424898634a1d
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-04-08 11:50:09 -0400

    Adjust styling for dropdown on mobile

--- src/components/typologyDropdown.jsx
+++ src/components/typologyDropdown.jsx
@@ -2,45 +2,45 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const TypologyDropdown = ({ updateTypology, currentTypology }) => {
-  const selectWidth = {
-    adu: 339,
-    tinyhouse: 159,
-    rowhouse: 157,
-    mixeduse: 505,
-    cottage: 307,
-    cohousing: 274,
-  };
   const updateSelectClassnames = (newTypology) => {
     document.querySelector('.dropdown__select').classList = [`dropdown__select dropdown__select--${newTypology}`];
     document.querySelector('.dropdown__shadow').classList = [`dropdown__shadow dropdown__shadow--${newTypology}`];
   };
+  const exploreText = {
+    adu: 'Explore an',
+    tinyhouse: 'Explore a',
+    rowhouse: 'Explore a',
+    mixeduse: 'Explore a',
+    cottage: 'Explore a',
+    cohousing: 'Explore',
+  };
 
   return (
-    <div className="typology-selector">
+    <div className="typology-selector__wrapper">
       <h2 className="h2 typology-selector__header" id="typology">
-        Explore
-        {' '}
-        {currentTypology === 'adu' ? 'an ' : 'a '}
+        {exploreText[currentTypology]}
       </h2>
-      <div className="dropdown__wrapper">
-        <select
-          className="dropdown__select"
-          onChange={(event) => {
-            updateTypology(event.target.value);
-            updateSelectClassnames(event.target.value);
-            history.pushState(null, '', `/#typology/${event.target.value}`);
-          }}
-          defaultValue={currentTypology}
-        >
-          <option value="adu">accessory dwelling unit</option>
-          <option value="tinyhouse">tiny house</option>
-          <option value="rowhouse">row house</option>
-          <option value="mixeduse">small scale mixed-use development</option>
-          <option value="cottage">cottage development</option>
-          <option value="cohousing">cohousing building</option>
-        </select>
+      <div className="dropdown__outer-wrapper">
+        <div className="dropdown__inner-wrapper">
+          <select
+            className={`dropdown__select dropdown__select--${currentTypology}`}
+            onChange={(event) => {
+              updateTypology(event.target.value);
+              updateSelectClassnames(event.target.value);
+              history.pushState(null, '', `/#typology/${event.target.value}`);
+            }}
+            defaultValue={currentTypology}
+          >
+            <option value="adu" className="dropdown__option">accessory dwelling unit</option>
+            <option value="tinyhouse" className="dropdown__option">tiny house</option>
+            <option value="rowhouse" className="dropdown__option">row house</option>
+            <option value="mixeduse" className="dropdown__option">small scale mixed-use development</option>
+            <option value="cottage" className="dropdown__option">cottage development</option>
+            <option value="cohousing" className="dropdown__option">cohousing</option>
+          </select>
+        </div>
+        <div className={`dropdown__shadow dropdown__shadow--${currentTypology}`} />
       </div>
-      <div className="dropdown__shadow" />
     </div>
   );
 };
