872b6c1b274d9499c7d7a227aa17f9ca1db6c2f1
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-04-14 11:29:20 -0400

    Display history paragraphs from json arrays

--- src/components/history.jsx
+++ src/components/history.jsx
@@ -3,23 +3,27 @@ import React from 'react';
 import SubtopicHeader from './subtopicHeader';
 import OpportunityButton from './opportunityButton';
 import opportunties from '../content/opportunities.json';
+import history from '../content/history.json';
 
-const History = ({ typology }) => (
-  <article className="article">
-    <SubtopicHeader title="History" highlightWidth={85} />
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <div className="opportunity__wrapper">
-      <OpportunityButton
-        title={opportunties[typology].history.titleOne}
-        description={opportunties[typology].history.textOne}
-      />
-      <OpportunityButton
-        title={opportunties[typology].history.titleTwo}
-        description={opportunties[typology].history.textTwo}
-      />
-    </div>
-  </article>
-);
+const History = ({ typology }) => {
+  const historyParagraphs = history[typology].map(paragraph => <p>{paragraph}</p>);
+  return (
+    <article className="article">
+      <SubtopicHeader title="History" highlightWidth={85} />
+      {historyParagraphs}
+      <div className="opportunity__wrapper">
+        <OpportunityButton
+          title={opportunties[typology].history.titleOne}
+          description={opportunties[typology].history.textOne}
+        />
+        <OpportunityButton
+          title={opportunties[typology].history.titleTwo}
+          description={opportunties[typology].history.textTwo}
+        />
+      </div>
+    </article>
+  );
+};
 
 History.propTypes = {
   typology: PropTypes.string.isRequired,
