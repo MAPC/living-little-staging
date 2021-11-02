61e626214c013c1d769d16c264c640d080ba4cde
Kimberly <kimberly@Administrators-MacBook-Pro.local>
2021-11-02 09:08:33 -0400

    Add and style types grid

--- src/components/typologyGrid.jsx
+++ src/components/typologyGrid.jsx
@@ -26,14 +26,53 @@ const TypologyGrid = () => {
         }
       }
     },
+    townhouse: file(relativePath: {eq: "grid/tinyhouse.jpg"}) {
+      childImageSharp {
+        fixed(width: 640, height: 457) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    },
+    sfc: file(relativePath: {eq: "grid/tinyhouse.jpg"}) {
+      childImageSharp {
+        fixed(width: 640, height: 457) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    },
+    mixeduse: file(relativePath: {eq: "grid/tinyhouse.jpg"}) {
+      childImageSharp {
+        fixed(width: 640, height: 457) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    },
+    cohousing: file(relativePath: {eq: "grid/tinyhouse.jpg"}) {
+      childImageSharp {
+        fixed(width: 640, height: 457) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    },
   }
 `);
   return (
     <section className="section main-column">
-      <h2 className="h2">What is living little?</h2>
-      <GridItem typology="adu" image={data.adu.childImageSharp.fixed} />
-      <GridItem typology="cottage" image={data.cottage.childImageSharp.fixed} />
-      <GridItem typology="tinyhouse" image={data.tinyhouse.childImageSharp.fixed} />
+      <div className="typology-grid-container">
+        <div className="typology-grid-text">
+          <h2 className="h2">What are the types of <br/>living little?</h2>
+          <div className="dropdown__shadow"></div>
+          <p>What some of the alternative housing opportunities available to meet an ever growing demand for housing in the region?</p>
+          <p>Here are seven alternative housing opportunities:</p>
+        </div>
+        <GridItem typology="adu" image={data.adu.childImageSharp.fixed} />
+        <GridItem typology="cottage" image={data.cottage.childImageSharp.fixed} />
+        <GridItem typology="tinyhouse" image={data.tinyhouse.childImageSharp.fixed} />
+        <GridItem typology="townhouse" image={data.townhouse.childImageSharp.fixed} />
+        <GridItem typology="sfc" image={data.sfc.childImageSharp.fixed} />
+        <GridItem typology="mixeduse" image={data.mixeduse.childImageSharp.fixed} />
+        <GridItem typology="cohousing" image={data.cohousing.childImageSharp.fixed} />
+      </div>
     </section>
   );
 }
