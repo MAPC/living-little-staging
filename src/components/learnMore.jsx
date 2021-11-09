35d8f991fe5d2611b95d6bfdbe9d1420a0b48fa7
Kimberly <kimberly@Administrators-MacBook-Pro.local>
2021-11-08 23:08:44 -0500

    Create About page header, style page, and add link to home

--- src/components/learnMore.jsx
+++ src/components/learnMore.jsx
@@ -6,7 +6,7 @@ const LearnMore = () => (
   <section className="section main-column">
     <p className="learn-more__text">Dig deeper into Living Little with these additional digital resources:</p>
     <div className="learn-more__row">
-      <CallToAction title="About Living Little" description="Read the project background" resource="/about" />
+      <CallToAction title="About Living Little" description="Read the project background" resource="/about/" />
       <CallToAction title="Full Report" description="Read the original Living Little report" resource={report} />
       <CallToAction title="One Small Question" description="Print all of the Small Questions about Living Little" resource={report} />
       <CallToAction title="Case Studies" description="Take a closer look at all of our case studies" resource={report} />
